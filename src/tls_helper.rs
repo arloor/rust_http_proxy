bef51733764a527e25d0cecfa5db5b2701a357e0
arloor <admin@arloor.com>
2023-05-21 22:55:05 +0800

    alpn

--- src/tls_helper.rs
+++ src/tls_helper.rs
@@ -12,11 +12,9 @@
 use std::fs::File;
 
 
-
 use std::sync::Arc;
+use log::info;
 use tokio_rustls::rustls::{Certificate, PrivateKey, ServerConfig};
-
-
 
 
 pub type Acceptor = tokio_rustls::TlsAcceptor;
@@ -25,7 +23,7 @@ fn tls_acceptor_impl(key: &String, cert: &String) -> Acceptor {
     tls_config(key, cert).into()
 }
 
-fn tls_config(key:&String, cert: &String ) -> Arc<ServerConfig> {
+fn tls_config(key: &String, cert: &String) -> Arc<ServerConfig> {
     use std::io::{self, BufReader};
     let certs = rustls_pemfile::certs(&mut BufReader::new(File::open(cert).unwrap()))
         .map(|mut certs| certs.drain(..).map(Certificate).collect()).unwrap();
@@ -34,25 +32,28 @@ fn tls_config(key:&String, cert: &String ) -> Arc<ServerConfig> {
     let mut keys: Vec<PrivateKey> = rustls_pemfile::rsa_private_keys(&mut BufReader::new(File::open(key).unwrap()))
         .map(|mut keys| keys.drain(..).map(PrivateKey).collect()).unwrap();
     // 读取 PKCS#8 格式 -----BEGIN PRIVATE KEY-----
-    if keys.len()==0 {
+    if keys.len() == 0 {
         keys = rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(File::open(key).unwrap()))
             .map(|mut keys| keys.drain(..).map(PrivateKey).collect()).unwrap();
     }
-    if keys.len()==0 {
+    if keys.len() == 0 {
         keys = rustls_pemfile::ec_private_keys(&mut BufReader::new(File::open(key).unwrap()))
             .map(|mut keys| keys.drain(..).map(PrivateKey).collect()).unwrap();
     }
 
+    let mut config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, keys.remove(0))
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))
+        .unwrap();
+    config.alpn_protocols=vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+    info!("{:?}",config);
     Arc::new(
-        ServerConfig::builder()
-            .with_safe_defaults()
-            .with_no_client_auth()
-            .with_single_cert(certs, keys.remove(0))
-            .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))
-            .unwrap(),
+        config,
     )
 }
 
-pub fn tls_acceptor(raw_key:&String,cert:&String) -> Acceptor {
+pub fn tls_acceptor(raw_key: &String, cert: &String) -> Acceptor {
     tls_acceptor_impl(raw_key, cert)
 }
