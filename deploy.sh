17488a16a5d607983d7001b9e6dfe532a0ca0ee4
arloor <admin@arloor.com>
2023-05-24 19:51:31 +0800

    README.md

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+scp root@hk.arloor.dev:/root/.acme.sh/arloor.dev/fullchain.cer cert.pem
+scp root@hk.arloor.dev:/root/.acme.sh/arloor.dev/arloor.dev.key privkey.pem
 ssh root@hk.arloor.dev "curl https://raw.githubusercontent.com/arloor/rust_http_proxy/master/rpm/genbin.sh -k 2>/dev/null |zsh"
 scp root@hk.arloor.dev:/usr/bin/rust_http_proxy ~/Downloads/rust_http_proxy
 scp root@hk.arloor.dev:/root/rpmbuild/RPMS/x86_64/rust_http_proxy-0.1-1.all.x86_64.rpm ~/Downloads/rust_http_proxy-0.1-1.all.x86_64.rpm
