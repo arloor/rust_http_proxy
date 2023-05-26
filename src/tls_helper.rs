a032d03ad2658aab02ee043979db8fb05137b983
arloor <admin@arloor.com>
2023-05-26 16:23:28 +0800

    重构

--- src/tls_helper.rs
+++ src/tls_helper.rs
@@ -1,18 +1,5 @@
-//！ # tls_helper
-// 在 OpenSSL 和其他加密库中，您可能会遇到两种格式的私钥：-----BEGIN RSA PRIVATE KEY----- 和 -----BEGIN PRIVATE KEY-----。这两种格式的主要区别在于它们的编码方式和包含的信息。
-//
-// -----BEGIN RSA PRIVATE KEY-----：
-// 这种格式表示私钥是按照 PKCS#1 标准编码的。它仅包含用于 RSA 算法的私钥信息，不包含其他元数据。文件的内容是一个以 Base64 编码的 DER（Distinguished Encoding Rules）表示的 ASN.1（Abstract Syntax Notation One）结构。通常，这种私钥格式仅适用于 RSA 密钥。
-//
-// -----BEGIN PRIVATE KEY-----：
-// 这种格式表示私钥是按照 PKCS#8 标准编码的。与 PKCS#1 不同，PKCS#8 可以用于多种类型的密钥（如 RSA、DSA、EC），并提供了更通用的编码结构。这种格式的私钥包含关于密钥类型和算法的附加信息。与 PKCS#1 类似，文件的内容也是一个以 Base64 编码的 DER 表示的 ASN.1 结构。
-//
-// 总结一下，-----BEGIN RSA PRIVATE KEY----- 是特定于 RSA 的 PKCS#1 格式的私钥，而 -----BEGIN PRIVATE KEY----- 是更通用的 PKCS#8 格式的私钥，可用于多种加密算法。尽管两者之间有区别，但在实际使用中，许多加密库和工具都可以处理这两种格式。
-
 use std::env;
 use std::fs::File;
-
-
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
 use log::{info, warn};
@@ -20,63 +7,31 @@ use rustls_pemfile::Item;
 use tls_listener::AsyncTls;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_rustls::rustls::{Certificate, PrivateKey, ServerConfig};
+use tokio_rustls::TlsAcceptor;
 
 
-pub type Acceptor = MyTlsAcceptor;
+pub type Acceptor = TlsAcceptorAdaptor;
 
-fn tls_acceptor_impl(key: &String, cert: &String) -> Acceptor {
-    info!("init TlsAcceptor {}",if timed_refresh_cert(){
-        format!("which will refresh every {} hours",TIMED_REFRESH_INTERVAL_SECS/60/60)
-    }else{
-        "".to_string()
-    });
-    MyTlsAcceptor {
-        tls_config: tls_config(&key, &cert).unwrap(),
-        key: key.to_string(),
-        cert: cert.to_string(),
-        refresh_time: SystemTime::now(),
-    }
-}
+// wrap error
+type Error = Box<dyn std::error::Error>;
 
-fn tls_config(key: &String, cert: &String) -> Option<Arc<ServerConfig>> {
+fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
     use std::io::{self, BufReader};
-    let key_file = if let Ok(file) = File::open(key) {
-        file
-    } else {
-        warn!("打开私钥文件失败 {}",key);
-        return None;
-    };
-    let cert_file = if let Ok(file) = File::open(cert) {
-        file
-    } else {
-        warn!("打开证书文件失败 {}",cert);
-        return None;
-    };
-    let certs = match rustls_pemfile::certs(&mut BufReader::new(cert_file))
-        .map(|mut certs| certs.drain(..).map(Certificate).collect()) {
-        Ok(certs) => certs,
-        Err(e) => {
-            warn!("读取证书失败 {} {:?}",cert,e);
-            return None;
-        }
-    };
-    let key = if let Ok(Some(item)) = rustls_pemfile::read_one(&mut BufReader::new(key_file)) {
-        match item {
+    let key_file = File::open(key)?;
+    let cert_file = File::open(cert)?;
+    let certs = rustls_pemfile::certs(&mut BufReader::new(cert_file))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect())?;
+    let key_option = rustls_pemfile::read_one(&mut BufReader::new(key_file))?;
+    let key = if let Some(key_item) = key_option {
+        match key_item {
             Item::PKCS8Key(bytes) => PrivateKey(bytes),
             Item::ECKey(bytes) => PrivateKey(bytes),
             Item::RSAKey(bytes) => PrivateKey(bytes),
-            Item::X509Certificate(_) => {
-                warn!("私钥文件放了个证书 {}",key);
-                return None;
-            }
-            _ => {
-                warn!("读取私钥失败 {}",key);
-                return None;
-            }
+            Item::X509Certificate(_) => return Err(io::Error::new(io::ErrorKind::InvalidData, "cert in private key file!".to_string()).into()),
+            _ => return Err(io::Error::new(io::ErrorKind::InvalidData, "error read private key".to_string()).into()),
         }
     } else {
-        warn!("读取私钥失败 {}",key);
-        return None;
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "error read private key".to_string()).into());
     };
 
 
@@ -89,17 +44,28 @@ fn tls_config(key: &String, cert: &String) -> Option<Arc<ServerConfig>> {
         Ok(mut config) => {
             config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
             info!("new config is {:?}",config);
-            Some(Arc::new(config))
+            Ok(Arc::new(config))
         }
         Err(e) => {
-            warn!("构造Rustls ServerConfig失败 {:?}",e);
-            None
+            Err(e.into())
         }
     }
 }
 
-pub fn tls_acceptor(raw_key: &String, cert: &String) -> Acceptor {
-    tls_acceptor_impl(raw_key, cert)
+pub fn tls_acceptor(raw_key: &String, cert: &String) -> Result<Acceptor, Error> {
+    info!("init TlsAcceptor {}",if timed_refresh_cert(){
+        format!("which will refresh every {} hours",TIMED_REFRESH_INTERVAL_SECS/60/60)
+    }else{
+        "".to_string()
+    });
+    let tls_config = tls_config(&raw_key, &cert)?;
+    let tls_acceptor_adaptor = TlsAcceptorAdaptor {
+        tls_acceptor: tls_config.into(),
+        key: raw_key.to_string(),
+        cert: cert.to_string(),
+        refresh_time: SystemTime::now(),
+    };
+    Ok(tls_acceptor_adaptor)
 }
 
 pub fn is_over_tls() -> bool {
@@ -114,8 +80,8 @@ fn timed_refresh_cert() -> bool {
 /// I provide this struct to read TLS cert and key at every time of accepting.
 /// I need this feature because my TLS cert is out of date every 3 months (a limit from acme.sh) and I don't want to restart my server at that situation.
 #[derive(Clone)]
-pub struct MyTlsAcceptor {
-    tls_config: Arc<ServerConfig>,
+pub struct TlsAcceptorAdaptor {
+    tls_acceptor: TlsAcceptor,
     key: String,
     cert: String,
     refresh_time: SystemTime,
@@ -124,43 +90,43 @@ pub struct MyTlsAcceptor {
 const TIMED_REFRESH_INTERVAL_SECS: u64 = 8 * 60 * 60;
 const NEXT_REFRESH_INTERVAL_SECS: u64 = 5 * 60;
 
-impl<C: AsyncRead + AsyncWrite + Unpin> AsyncTls<C> for MyTlsAcceptor {
+impl<C: AsyncRead + AsyncWrite + Unpin> AsyncTls<C> for TlsAcceptorAdaptor {
     type Stream = tokio_rustls::server::TlsStream<C>;
     type Error = std::io::Error;
     type AcceptFuture = tokio_rustls::Accept<C>;
     fn accept(&self, conn: C) -> Self::AcceptFuture {
         let now = SystemTime::now();
         let second_since_last_refresh = now.duration_since(self.refresh_time).unwrap_or(Duration::from_secs(0)).as_secs();
-        let tls_config = if timed_refresh_cert() && second_since_last_refresh >= TIMED_REFRESH_INTERVAL_SECS {
+        let tls_acceptor = if timed_refresh_cert() && second_since_last_refresh >= TIMED_REFRESH_INTERVAL_SECS {
             self.refresh_and_return_tls_config(now)
         } else {
-            self.tls_config.clone()
+            self.tls_acceptor.clone()
         };
-        tokio_rustls::TlsAcceptor::accept(&tls_config.clone().into(), conn)
+        TlsAcceptor::accept(&tls_acceptor, conn)
     }
 }
 
-impl MyTlsAcceptor {
-    fn refresh_and_return_tls_config(&self, now: SystemTime) -> Arc<ServerConfig> {
+impl TlsAcceptorAdaptor {
+    fn refresh_and_return_tls_config(&self, now: SystemTime) -> TlsAcceptor {
         match tls_config(&self.key, &self.cert) {
-            Some(tls_config) => {
+            Ok(tls_config) => {
                 // 使用unsafe更新不可变对象的字段
                 unsafe {
-                    let tls_config_ptr: *mut Arc<ServerConfig> = &self.tls_config as *const _ as *mut _;
-                    *tls_config_ptr = tls_config.clone();
+                    let tls_config_ptr: *mut TlsAcceptor = &self.tls_acceptor as *const _ as *mut _;
+                    *tls_config_ptr = tls_config.clone().into();
                     let refresh_time_ptr: *mut SystemTime = &self.refresh_time as *const _ as *mut _;
                     *refresh_time_ptr = now;
                 }
-                tls_config.clone()
+                tls_config.into()
             }
-            None => {
-                warn!("error refresh cert, will refresh in {} seconds",NEXT_REFRESH_INTERVAL_SECS);
+            Err(e) => {
+                warn!("error refresh cert, error: {}, will refresh in {} seconds",e,NEXT_REFRESH_INTERVAL_SECS);
                 // 使用unsafe更新不可变对象的字段
                 unsafe {
                     let refresh_time_ptr: *mut SystemTime = &self.refresh_time as *const _ as *mut _;
                     *refresh_time_ptr = now - Duration::from_secs(TIMED_REFRESH_INTERVAL_SECS) + Duration::from_secs(NEXT_REFRESH_INTERVAL_SECS);
                 }
-                self.tls_config.clone()
+                self.tls_acceptor.clone()
             }
         }
     }
