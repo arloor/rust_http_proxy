08a3eaa8a7ce03b05362cfa343197fce863c06bb
arloor <admin@arloor.com>
2023-06-05 23:54:35 +0800

    [Important] Rust 1.70开始，之前的unsafe代码处会core dump。改为每次建连都读取证书，从而去掉之前的unsafe代码

--- src/tls_helper.rs
+++ src/tls_helper.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::sync::Arc;
-use std::time::{Duration, SystemTime};
-use log::{info, warn};
+use log::{warn};
 use rustls_pemfile::Item;
 use tls_listener::AsyncTls;
 use tokio::io::{AsyncRead, AsyncWrite};
@@ -43,7 +42,7 @@ fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
     {
         Ok(mut config) => {
             config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
-            info!("new config is {:?}",config);
+            // info!("new config is {:?}",config);
             Ok(Arc::new(config))
         }
         Err(e) => {
@@ -53,17 +52,11 @@ fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
 }
 
 pub fn tls_acceptor(raw_key: &String, cert: &String) -> Result<Acceptor, Error> {
-    info!("init TlsAcceptor {}",if timed_refresh_cert(){
-        format!("which will refresh every {} hours",TIMED_REFRESH_INTERVAL_SECS/60/60)
-    }else{
-        "".to_string()
-    });
     let tls_config = tls_config(&raw_key, &cert)?;
     let tls_acceptor_adaptor = TlsAcceptorAdaptor {
         tls_acceptor: tls_config.into(),
         key: raw_key.to_string(),
         cert: cert.to_string(),
-        refresh_time: SystemTime::now(),
     };
     Ok(tls_acceptor_adaptor)
 }
@@ -72,9 +65,6 @@ pub fn is_over_tls() -> bool {
     "true" == env::var("over_tls").unwrap_or("false".to_string())
 }
 
-fn timed_refresh_cert() -> bool {
-    "true" == env::var("timed_refresh_cert").unwrap_or("true".to_string())
-}
 
 /// modified from `tokio_rustls::TlsAcceptor`, which is a wrapper around a `rustls::ServerConfig`, providing an async `accept` method.
 /// I provide this struct to read TLS cert and key at every time of accepting.
@@ -84,47 +74,18 @@ pub struct TlsAcceptorAdaptor {
     tls_acceptor: TlsAcceptor,
     key: String,
     cert: String,
-    refresh_time: SystemTime,
 }
-
-const TIMED_REFRESH_INTERVAL_SECS: u64 = 24 * 60 * 60;
-const NEXT_REFRESH_INTERVAL_SECS: u64 = TIMED_REFRESH_INTERVAL_SECS / 2;
 
 impl<C: AsyncRead + AsyncWrite + Unpin> AsyncTls<C> for TlsAcceptorAdaptor {
     type Stream = tokio_rustls::server::TlsStream<C>;
     type Error = std::io::Error;
     type AcceptFuture = tokio_rustls::Accept<C>;
     fn accept(&self, conn: C) -> Self::AcceptFuture {
-        self.refresh_if_need();
-        TlsAcceptor::accept(&self.tls_acceptor, conn)
-    }
-}
-
-impl TlsAcceptorAdaptor {
-    fn refresh_if_need(&self) {
-        if timed_refresh_cert() {
-            let now = SystemTime::now();
-            let second_since_last_refresh = now.duration_since(self.refresh_time).unwrap_or(Duration::from_secs(0)).as_secs();
-            if second_since_last_refresh >= TIMED_REFRESH_INTERVAL_SECS {
-                match tls_config(&self.key, &self.cert) {
-                    Ok(tls_config) => {
-                        // 使用unsafe更新不可变对象的字段
-                        unsafe {
-                            let tls_config_ptr: *mut TlsAcceptor = &self.tls_acceptor as *const _ as *mut _;
-                            *tls_config_ptr = tls_config.clone().into();
-                            let refresh_time_ptr: *mut SystemTime = &self.refresh_time as *const _ as *mut _;
-                            *refresh_time_ptr = now;
-                        }
-                    }
-                    Err(e) => {
-                        warn!("error refresh cert, error: {}, will refresh in {} seconds",e,NEXT_REFRESH_INTERVAL_SECS);
-                        // 使用unsafe更新不可变对象的字段
-                        unsafe {
-                            let refresh_time_ptr: *mut SystemTime = &self.refresh_time as *const _ as *mut _;
-                            *refresh_time_ptr = now - Duration::from_secs(TIMED_REFRESH_INTERVAL_SECS) + Duration::from_secs(NEXT_REFRESH_INTERVAL_SECS);
-                        }
-                    }
-                }
+        match tls_config(&self.key, &self.cert) {
+            Ok(tls_config) => TlsAcceptor::accept(&tls_config.clone().into(), conn),
+            Err(e) => {
+                warn!("error read current cert, {:?}",e);
+                TlsAcceptor::accept(&self.tls_acceptor, conn)
             }
         }
     }
