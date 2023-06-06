b28e36d2a0b8757b0601e0a81dd37f16948d965b
arloor <admin@arloor.com>
2023-06-06 14:59:28 +0800

    去除Tls-listener，使用hyper-rustls的acceptor.rs

--- src/tls_helper.rs
+++ src/tls_helper.rs
@@ -1,20 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::sync::Arc;
-use log::{warn};
 use rustls_pemfile::Item;
-use tls_listener::AsyncTls;
-use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_rustls::rustls::{Certificate, PrivateKey, ServerConfig};
-use tokio_rustls::TlsAcceptor;
 
 
-pub type Acceptor = TlsAcceptorAdaptor;
 
 // wrap error
 type Error = Box<dyn std::error::Error>;
 
-fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
+pub fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
     use std::io::{self, BufReader};
     let key_file = File::open(key)?;
     let cert_file = File::open(cert)?;
@@ -51,42 +46,7 @@ fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Error> {
     }
 }
 
-pub fn tls_acceptor(raw_key: &String, cert: &String) -> Result<Acceptor, Error> {
-    let tls_config = tls_config(&raw_key, &cert)?;
-    let tls_acceptor_adaptor = TlsAcceptorAdaptor {
-        tls_acceptor: tls_config.into(),
-        key: raw_key.to_string(),
-        cert: cert.to_string(),
-    };
-    Ok(tls_acceptor_adaptor)
-}
-
 pub fn is_over_tls() -> bool {
     "true" == env::var("over_tls").unwrap_or("false".to_string())
 }
 
-
-/// modified from `tokio_rustls::TlsAcceptor`, which is a wrapper around a `rustls::ServerConfig`, providing an async `accept` method.
-/// I provide this struct to read TLS cert and key at every time of accepting.
-/// I need this feature because my TLS cert is out of date every 3 months (a limit from acme.sh) and I don't want to restart my server at that situation.
-#[derive(Clone)]
-pub struct TlsAcceptorAdaptor {
-    tls_acceptor: TlsAcceptor,
-    key: String,
-    cert: String,
-}
-
-impl<C: AsyncRead + AsyncWrite + Unpin> AsyncTls<C> for TlsAcceptorAdaptor {
-    type Stream = tokio_rustls::server::TlsStream<C>;
-    type Error = std::io::Error;
-    type AcceptFuture = tokio_rustls::Accept<C>;
-    fn accept(&self, conn: C) -> Self::AcceptFuture {
-        match tls_config(&self.key, &self.cert) {
-            Ok(tls_config) => TlsAcceptor::accept(&tls_config.clone().into(), conn),
-            Err(e) => {
-                warn!("error read current cert, {:?}",e);
-                TlsAcceptor::accept(&self.tls_acceptor, conn)
-            }
-        }
-    }
-}
