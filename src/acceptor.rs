3b5a5e3cd8ede13258e778274e9cb2734566806c
arloor <admin@arloor.com>
2023-06-06 15:46:41 +0800

    每秒钟刷新一次证书

--- src/acceptor.rs
+++ src/acceptor.rs
@@ -1,18 +1,22 @@
 // https://github.com/rustls/hyper-rustls/blob/286e1fa57ff5cac99994fab355f91c3454d6d83d/src/acceptor.rs
 use core::task::{Context, Poll};
 use std::future::Future;
-use std::io;
+use std::{env, io};
+use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::sync::Arc;
+use std::time::{Duration, SystemTime};
 
 use futures_util::ready;
 use hyper::server::{
     accept::Accept,
     conn::{AddrIncoming, AddrStream},
 };
+use log::{info, warn};
 use rustls::ServerConfig;
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use crate::tls_helper::tls_config;
 
 
 enum State {
@@ -105,38 +109,59 @@ impl AsyncWrite for TlsStream {
 
 /// A TLS acceptor that can be used with hyper servers.
 pub struct TlsAcceptor {
+    key: String,
+    cert: String,
     config: Arc<ServerConfig>,
     incoming: AddrIncoming,
+    last_refresh_time: SystemTime,
 }
 
 /// An Acceptor for the `https` scheme.
 impl TlsAcceptor {
     /// Provides a builder for a `TlsAcceptor`.
     /// Creates a new `TlsAcceptor` from a `ServerConfig` and an `AddrIncoming`.
-    pub fn new(config: Arc<ServerConfig>, incoming: AddrIncoming) -> TlsAcceptor {
-        TlsAcceptor { config, incoming }
+
+
+    pub fn new(key: String, cert: String, incoming: AddrIncoming) -> Result<TlsAcceptor, Box<dyn std::error::Error>> {
+        match tls_config(&key, &cert) {
+            Ok(config) => {
+                let acceptor = TlsAcceptor { key, cert, config, incoming, last_refresh_time: SystemTime::now() };
+                return Ok(acceptor);
+            }
+            Err(e) => return Err(e)
+        }
+    }
+
+    fn refresh_if_need(&mut self) {
+        let now = SystemTime::now();
+        if now.duration_since(self.last_refresh_time).unwrap_or(Duration::from_secs(0)) > Duration::from_secs(REFRESH_TIME) {
+            self.last_refresh_time = now;
+            match tls_config(&self.key, &self.cert) {
+                Ok(config) => {
+                    info!("success refresh certs!");
+                    self.config = config
+                }
+                Err(e) => {
+                    warn!("error refresh certs, {:?}",e);
+                }
+            }
+        }
     }
 }
 
-impl<C, I> From<(C, I)> for TlsAcceptor
-    where
-        C: Into<Arc<ServerConfig>>,
-        I: Into<AddrIncoming>,
-{
-    fn from((config, incoming): (C, I)) -> TlsAcceptor {
-        TlsAcceptor::new(config.into(), incoming.into())
-    }
-}
+// 每小时更新证书
+const REFRESH_TIME: u64 = 1;
 
 impl Accept for TlsAcceptor {
     type Conn = TlsStream;
-    type Error = io::Error;
+    type Error = Error;
 
     fn poll_accept(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
         let pin = self.get_mut();
+        pin.refresh_if_need();
         match ready!(Pin::new(&mut pin.incoming).poll_accept(cx)) {
             Some(Ok(sock)) => Poll::Ready(Some(Ok(TlsStream::new(sock, pin.config.clone())))),
             Some(Err(e)) => Poll::Ready(Some(Err(e))),
