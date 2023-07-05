4e7e12613966f3532b9dd7fc158ca837dc83e95d
arloor <admin@arloor.com>
2023-07-05 10:45:04 +0800

    增加日志

--- podman.md
+++ podman.md
@@ -1,7 +1,7 @@
 
 ```shell
 podman build -t rust_http_proxy .
-podman run -d --env-file /etc/rust_http_proxy/env -p 443:443 -v /root/.acme.sh/arloor.dev/:/root/.acme.sh/arloor.dev/ -v /usr/share/nginx/html/:/usr/share/nginx/html/ -v /tmp:/tmp --name proxy rust_http_proxy
+podman run -d --env-file /etc/rust_http_proxy/env -p 8443:443 -v /root/.acme.sh/arloor.dev/:/root/.acme.sh/arloor.dev/ -v /usr/share/nginx/html/:/usr/share/nginx/html/ -v /tmp:/tmp --name proxy docker.io/arloor/rust_http_proxy
 podman exec -it proxy /bin/sh
 podman kill proxy
 podman rm --all
--- src/main.rs
+++ src/main.rs
@@ -130,12 +130,19 @@ async fn proxy(client: &HttpClient, mut req: Request<Body>, basic_auth: &String,
         };
     }
 
+
     if basic_auth.len() != 0 { //需要检验鉴权
         let mut authed: bool = false;
-        if let Some(header) = req.headers().get(http::header::PROXY_AUTHORIZATION) {
+        let header_option = req.headers().get(http::header::PROXY_AUTHORIZATION);
+        if header_option.is_none() {
+            warn!("no PROXY_AUTHORIZATION from {:?}",client_socket_addr)
+        } else {
+            let header = header_option.unwrap();
             if let Ok(base64) = header.to_str() {
                 if base64 == *basic_auth {
                     authed = true;
+                } else {
+                    warn!("wrong PROXY_AUTHORIZATION from {:?}, {:?}",client_socket_addr,base64)
                 }
             }
         }
