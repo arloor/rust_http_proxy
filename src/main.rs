94714b701cf80a6f9fc3678511a2654c200c9a67
arloor <admin@arloor.com>
2023-12-15 00:50:11 +0800

    auto server

--- src/main.rs
+++ src/main.rs
@@ -111,8 +111,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         let tcp_listener = TcpListener::bind(addr).await?;
         loop {
-            let (tcp_stream, _) = tcp_listener.accept().await?;
-            let client_socket_addr = tcp_stream.peer_addr()?;
+            let (tcp_stream, client_socket_addr) = tcp_listener.accept().await?;
+            // let client_socket_addr = tcp_stream.peer_addr()?;
             let io = TokioIo::new(tcp_stream);
             tokio::task::spawn(async move {
                 let connection = http1::Builder::new()
