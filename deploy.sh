050c5e59ffeb3ba0cc17cb602f2194b3bdcf3bdf
arloor <admin@arloor.com>
2023-12-21 21:32:24 +0800

    Update deploy.sh script to include podman rmi command

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 hosts="hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev gg.arloor.dev ti.arloor.dev"
 for i in ${hosts};
 do
-    ssh root@${i} 'hostname;systemctl restart proxy'
+    ssh root@${i} 'hostname;systemctl restart proxy;podman rmi -a 2>/dev/null'
 done
 
-ssh root@us.arloor.dev 'hostname;systemctl restart guest'
+ssh root@us.arloor.dev 'hostname;systemctl restart guest;podman rmi -a 2>/dev/null'
