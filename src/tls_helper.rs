1abdce7ced4301dd4a98c38cb78b8d8c23d8475d
刘港欢 <admin@arloor.com>
2023-12-23 09:40:21 +0000

    Update dependencies in Cargo.toml and tls_helper.rs

--- src/tls_helper.rs
+++ src/tls_helper.rs
@@ -1,7 +1,9 @@
 use rustls_pemfile::Item;
 use std::fs::File;
 use std::sync::Arc;
-use tokio_rustls::rustls::{Certificate, PrivateKey, ServerConfig};
+use tokio_rustls::rustls::pki_types::CertificateDer;
+use tokio_rustls::rustls::pki_types::PrivateKeyDer;
+use tokio_rustls::rustls::ServerConfig;
 
 // wrap error
 type Error = Box<dyn std::error::Error>;
@@ -16,14 +18,15 @@ pub fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Erro
         File::open(key).map_err(|_| <&str as Into<Error>>::into("open private key failed"))?;
     let cert_file =
         File::open(cert).map_err(|_| <&str as Into<Error>>::into("open cert failed"))?;
-    let certs = rustls_pemfile::certs(&mut BufReader::new(cert_file))
-        .map(|mut certs| certs.drain(..).map(Certificate).collect())?;
+    let certs: io::Result<Vec<CertificateDer<'static>>> =
+        rustls_pemfile::certs(&mut BufReader::new(cert_file))
+            .collect::<io::Result<Vec<CertificateDer<'static>>>>();
     let key_option = rustls_pemfile::read_one(&mut BufReader::new(key_file))?;
     let key = if let Some(key_item) = key_option {
         match key_item {
-            Item::PKCS8Key(bytes) => PrivateKey(bytes),
-            Item::ECKey(bytes) => PrivateKey(bytes),
-            Item::RSAKey(bytes) => PrivateKey(bytes),
+            Item::Pkcs8Key(bytes) => PrivateKeyDer::Pkcs8(bytes),
+            Item::Pkcs1Key(bytes) => PrivateKeyDer::Pkcs1(bytes),
+            Item::Sec1Key(bytes) => PrivateKeyDer::Sec1(bytes),
             Item::X509Certificate(_) => return Err("cert in private key file".into()),
             _ => {
                 return Err(io::Error::new(
@@ -38,9 +41,8 @@ pub fn tls_config(key: &String, cert: &String) -> Result<Arc<ServerConfig>, Erro
     };
 
     match ServerConfig::builder()
-        .with_safe_defaults()
         .with_no_client_auth()
-        .with_single_cert(certs, key)
+        .with_single_cert(certs?, key)
         .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))
     {
         Ok(mut config) => {
