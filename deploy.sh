53cc42c26b37821f164ee98ab89f764c153a8e15
刘港欢 <admin@arloor.com>
2023-12-23 10:12:00 +0000

    Add SSH keyscan to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 hosts="ti.arloor.dev hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev gg.arloor.dev bwg.arloor.dev"
 for i in ${hosts}; do
+    ssh-keyscan -H ${i} > ~/.ssh/known_hosts
     ssh root@${i} '
             source /etc/profile
             echo $http_proxy
@@ -9,7 +10,7 @@ for i in ${hosts}; do
             podman rmi -a 2>/dev/null
             '
 done
-
+ssh-keyscan -H ${i} > ~/.ssh/known_hosts
 ssh root@us.arloor.dev '
             source /etc/profile
             echo $http_proxy
