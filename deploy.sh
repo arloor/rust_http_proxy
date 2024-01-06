4344228183d280a15d0dcd32f08f5ec4c7e4bdc9
arloor <admin@arloor.com>
2024-01-06 20:35:35 +0800

    Update deploy.sh with new host li.arloor.com

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-hosts="hk.arloor.dev hkg.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev gg.arloor.dev bwg.arloor.dev ti.arloor.com"
+hosts="hk.arloor.dev hkg.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev gg.arloor.dev bwg.arloor.dev ti.arloor.com li.arloor.com"
 # echo "" > ~/.ssh/known_hosts
 # for i in ${hosts}; do
 #     ssh-keyscan -H ${i} >> ~/.ssh/known_hosts
