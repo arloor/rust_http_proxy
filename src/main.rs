f89b04bd9657413f92b7062ed54418d837433241
arloor <admin@arloor.com>
2024-01-30 17:56:59 +0800

    Revert "不使用宏"
    This reverts commit 4a7943b2a28fed649b92d32d3325081664df18b2.

--- src/main.rs
+++ src/main.rs
@@ -117,53 +117,7 @@ async fn serve(
                             let io = TokioIo::new(conn);
                             let proxy_handler=proxy_handler.clone();
                             tokio::spawn(async move {
-                                let binding =auto::Builder::new(hyper_util::rt::tokio::TokioExecutor::new());
-        let context=Arc::new(RwLock::new(Context::default()));
-        let context_c=context.clone();
-        let connection =
-            binding.serve_connection_with_upgrades(io, service_fn(move |req| {
-                proxy(
-                    req,
-                    config,
-                    client_socket_addr,
-                    proxy_handler.clone(),
-                    context.clone()
-                )
-            }));
-        tokio::pin!(connection);
-        loop {
-            let (last_instant,upgraded) = context_c.read().unwrap().snapshot();
-            if upgraded {
-                tokio::select! {
-                    res = connection.as_mut() => {
-                        if let Err(err)=res{
-                            handle_hyper_error(client_socket_addr,err);
-                        }
-                        break;
-                    }
-                }
-            } else {
-                tokio::select! {
-                    res = connection.as_mut() => {
-                        if let Err(err)=res{
-                            handle_hyper_error(client_socket_addr,err);
-                        }
-                        break;
-                    }
-                    _ = tokio::time::sleep_until(last_instant+Duration::from_secs(IDLE_SECONDS)) => {
-                        let (instant,upgraded) = context_c.read().unwrap().snapshot();
-                        if upgraded {
-                            info!("upgraded from {}",client_socket_addr);
-                            continue;
-                        }else if instant <= last_instant {
-                            info!("idle for {} seconds, graceful shutdown [{}]",IDLE_SECONDS,client_socket_addr);
-                            connection.as_mut().graceful_shutdown();
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+                                serve_with_idle_timeout!(io,proxy_handler,config,client_socket_addr);
                             });
                         }
                         Err(err) => {
@@ -180,53 +134,7 @@ async fn serve(
                 let io = TokioIo::new(tcp_stream);
                 let proxy_handler = proxy_handler.clone();
                 tokio::task::spawn(async move {
-                    let binding =auto::Builder::new(hyper_util::rt::tokio::TokioExecutor::new());
-        let context=Arc::new(RwLock::new(Context::default()));
-        let context_c=context.clone();
-        let connection =
-            binding.serve_connection_with_upgrades(io, service_fn(move |req| {
-                proxy(
-                    req,
-                    config,
-                    client_socket_addr,
-                    proxy_handler.clone(),
-                    context.clone()
-                )
-            }));
-        tokio::pin!(connection);
-        loop {
-            let (last_instant,upgraded) = context_c.read().unwrap().snapshot();
-            if upgraded {
-                tokio::select! {
-                    res = connection.as_mut() => {
-                        if let Err(err)=res{
-                            handle_hyper_error(client_socket_addr,err);
-                        }
-                        break;
-                    }
-                }
-            } else {
-                tokio::select! {
-                    res = connection.as_mut() => {
-                        if let Err(err)=res{
-                            handle_hyper_error(client_socket_addr,err);
-                        }
-                        break;
-                    }
-                    _ = tokio::time::sleep_until(last_instant+Duration::from_secs(IDLE_SECONDS)) => {
-                        let (instant,upgraded) = context_c.read().unwrap().snapshot();
-                        if upgraded {
-                            info!("upgraded from {}",client_socket_addr);
-                            continue;
-                        }else if instant <= last_instant {
-                            info!("idle for {} seconds, graceful shutdown [{}]",IDLE_SECONDS,client_socket_addr);
-                            connection.as_mut().graceful_shutdown();
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+                    serve_with_idle_timeout!(io, proxy_handler, config, client_socket_addr);
                 });
             }
         }
