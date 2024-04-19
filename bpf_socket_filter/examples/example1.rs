269b6df46a495f1a4bdb58928fe23531f0167e33
arloor <admin@arloor.com>
2024-04-19 17:29:57 +0800

    Add debug flag to rust.yml workflow and update socket_filter initialization in example1.rs

--- bpf_socket_filter/examples/example1.rs
+++ bpf_socket_filter/examples/example1.rs
@@ -3,7 +3,7 @@ use std::{thread::sleep, time::Duration};
 extern crate bpf_socket_filter as socket_filter;
 
 fn main() {
-    let mut socket_filter = socket_filter::SocketFilter::default();
+    let socket_filter = socket_filter::SocketFilter::default();
     loop{
         let value = socket_filter.get_value();
         println!("{}",value);
