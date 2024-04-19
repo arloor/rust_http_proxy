8348aa06510b4344e25794ae81ae9b869af264e5
arloor <admin@arloor.com>
2024-04-19 17:05:09 +0800

    将bpf socket filter作为feature

--- rust_http_proxy/src/net_monitor.rs
+++ rust_http_proxy/src/net_monitor.rs
@@ -16,10 +16,13 @@ impl TimeValue {
         TimeValue { time, value }
     }
 }
+
+#[cfg(feature = "bpf")]
 use bpf_socket_filter::SocketFilter;
 #[derive(Clone)]
 pub struct NetMonitor {
     buffer: Arc<RwLock<VecDeque<TimeValue>>>,
+    #[cfg(feature = "bpf")]
     socket_filter:Arc<SocketFilter>,
 }
 
@@ -27,6 +30,7 @@ impl NetMonitor {
     pub fn new() -> NetMonitor {
         NetMonitor {
             buffer: Arc::new(RwLock::new(VecDeque::<TimeValue>::new())),
+            #[cfg(feature = "bpf")]
             socket_filter:Arc::new(SocketFilter::default()),
         }
     }
@@ -37,12 +41,16 @@ impl NetMonitor {
     pub fn start(&self) {
         if cfg!(target_os = "linux") {
             let to_move = self.buffer.clone();
+            #[cfg(feature = "bpf")]
             let socket_filter = self.socket_filter.clone();
             tokio::spawn(async move {
                 let mut last: u64 = 0;
                 loop {
                     {
+                        #[cfg(feature = "bpf")]
                         let new = socket_filter.get_value();
+                        #[cfg(not(feature = "bpf"))]
+                        let new = fetch_current_value();
                         if last != 0 {
                             let system_time = SystemTime::now();
                             let datetime: DateTime<Local> = system_time.into();
@@ -69,40 +77,40 @@ const MAX_NUM: usize = 300;
 
 
 // Inter-|   Receive                                                |  Transmit
-    //  face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
-    //     lo: 199123505  183957    0    0    0     0          0         0 199123505  183957    0    0    0     0       0          0
-    //   ens5: 194703959  424303    0    0    0     0          0         0 271636211  425623    0    0    0     0       0          0
-// #[cfg(not(feature = "bpf"))]
-// fn fetch_current_value(net_monitor:&NetMonitor) -> u64 {
-//     use std::fs;
-//     if let Ok(mut content) = fs::read_to_string("/proc/net/dev") {
-//         content = content.replace("\r\n", "\n");
-//         let strs = content.split('\n');
-//         let mut new: u64 = 0;
-//         for str in strs {
-//             let array: Vec<&str> = str.split_whitespace().collect();
+//      face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+//         lo: 199123505  183957    0    0    0     0          0         0 199123505  183957    0    0    0     0       0          0
+//       ens5: 194703959  424303    0    0    0     0          0         0 271636211  425623    0    0    0     0       0          0
+#[cfg(not(feature = "bpf"))]
+fn fetch_current_value() -> u64 {
+    use std::fs;
+    if let Ok(mut content) = fs::read_to_string("/proc/net/dev") {
+        content = content.replace("\r\n", "\n");
+        let strs = content.split('\n');
+        let mut new: u64 = 0;
+        for str in strs {
+            let array: Vec<&str> = str.split_whitespace().collect();
 
-//             if array.len() == 17 {
-//                 if *array.first().unwrap_or(&"") == "lo:" {
-//                     continue;
-//                 }
-//                 if array.first().unwrap_or(&"").starts_with("veth") {
-//                     continue;
-//                 }
-//                 if array.first().unwrap_or(&"").starts_with("flannel") {
-//                     continue;
-//                 }
-//                 if array.first().unwrap_or(&"").starts_with("cni0") {
-//                     continue;
-//                 }
-//                 if array.first().unwrap_or(&"").starts_with("utun") {
-//                     continue;
-//                 }
-//                 new += array.get(9).unwrap_or(&"").parse::<u64>().unwrap_or(0);
-//             }
-//         }
-//         new
-//     } else {
-//         0
-//     }
-// }
+            if array.len() == 17 {
+                if *array.first().unwrap_or(&"") == "lo:" {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("veth") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("flannel") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("cni0") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("utun") {
+                    continue;
+                }
+                new += array.get(9).unwrap_or(&"").parse::<u64>().unwrap_or(0);
+            }
+        }
+        new
+    } else {
+        0
+    }
+}
