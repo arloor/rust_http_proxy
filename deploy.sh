b8e7495c3edf49ea70853ee1c817630790f3ea25
arloor <admin@arloor.com>
2024-04-19 22:01:29 +0800

    Update hosts in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ for i in ${hosts}; do
             '
 done
 
-hosts="hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev bwg.arloor.dev pl.arloor.com ti.arloor.com li.arloor.dev"
+hosts="hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev bwg.arloor.dev"
 for i in ${hosts}; do
     ssh -o StrictHostKeyChecking=no root@${i} '
             hostname;
@@ -18,3 +18,15 @@ for i in ${hosts}; do
             systemctl restart rust_http_proxy;
             '
 done
+
+hosts="pl.arloor.com ti.arloor.com li.arloor.dev"
+for i in ${hosts}; do
+    ssh -o StrictHostKeyChecking=no root@${i} '
+            hostname;
+            source /etc/profile
+            curl -SLf https://github.com/arloor/rust_http_proxy/releases/download/vebpf/rust_http_proxy -o /tmp/aasdasdada
+            install /tmp/aasdasdada /usr/bin/rust_http_proxy&&rm -f /tmp/aasdasdada
+            systemctl restart rust_http_proxy;
+            '
+done
+
