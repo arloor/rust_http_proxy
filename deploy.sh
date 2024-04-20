9da259dee420e338475ac0973dc4fbeb3a1781a0
arloor <admin@arloor.com>
2024-04-20 17:20:45 +0800

    Update deploy.sh script to include additional hosts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-hosts="gg.arloor.dev"
+hosts="hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev bwg.arloor.dev gg.arloor.dev pl.arloor.com ti.arloor.com li.arloor.dev"
 for i in ${hosts}; do
     ssh -o StrictHostKeyChecking=no root@${i} '
             hostname;
@@ -8,25 +8,3 @@ for i in ${hosts}; do
             podman images --digests |grep arloor/rust_http_proxy|awk "{print \$4\" \"\$3}";
             '
 done
-
-hosts="hk.arloor.dev sg.arloor.dev di.arloor.dev us.arloor.dev bwg.arloor.dev"
-for i in ${hosts}; do
-    ssh -o StrictHostKeyChecking=no root@${i} '
-            hostname;
-            curl -SLf https://github.com/arloor/rust_http_proxy/releases/download/vebpf/rust_http_proxy -o /tmp/aasdasdada
-            install /tmp/aasdasdada /usr/bin/rust_http_proxy&&rm -f /tmp/aasdasdada
-            systemctl restart rust_http_proxy;
-            '
-done
-
-hosts="pl.arloor.com ti.arloor.com li.arloor.dev"
-for i in ${hosts}; do
-    ssh -o StrictHostKeyChecking=no root@${i} '
-            hostname;
-            source /etc/profile
-            curl -SLf https://github.com/arloor/rust_http_proxy/releases/download/vebpf/rust_http_proxy -o /tmp/aasdasdada
-            install /tmp/aasdasdada /usr/bin/rust_http_proxy&&rm -f /tmp/aasdasdada
-            systemctl restart rust_http_proxy;
-            '
-done
-
