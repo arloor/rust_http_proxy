83f36f5a202754d05b8cccad1a86fc2c2f560359
arloor <admin@arloor.com>
2024-06-14 18:33:11 +0800

    Refactor NetMonitor to use async/await for fetching data

--- rust_http_proxy/src/net_monitor.rs
+++ rust_http_proxy/src/net_monitor.rs
@@ -18,13 +18,16 @@ impl TimeValue {
 }
 
 #[cfg(feature = "bpf")]
+use lazy_static::lazy_static;
+#[cfg(feature = "bpf")]
 use socket_filter::SocketFilter;
+#[cfg(feature = "bpf")]
+lazy_static! {
+    static ref SOCKET_FILTER: Arc<SocketFilter> = Arc::new(SocketFilter::default());
+}
 
-#[derive(Clone)]
 pub struct NetMonitor {
     buffer: Arc<RwLock<VecDeque<TimeValue>>>,
-    #[cfg(feature = "bpf")]
-    socket_filter: Arc<SocketFilter>,
 }
 const TOTAL_SECONDS: u64 = 900;
 const INTERVAL_SECONDS: u64 = 5;
@@ -33,14 +36,11 @@ impl NetMonitor {
     pub fn new() -> NetMonitor {
         NetMonitor {
             buffer: Arc::new(RwLock::new(VecDeque::<TimeValue>::new())),
-            #[cfg(feature = "bpf")]
-            socket_filter: Arc::new(SocketFilter::default()),
         }
     }
 
     pub async fn fetch_all(&self) -> Vec<TimeValue> {
-        let buffer = self.buffer.clone();
-        let buffer = buffer.read().await;
+        let buffer = self.buffer.read().await;
         let x = buffer.as_slices();
         let mut r = vec![];
         r.extend_from_slice(x.0);
@@ -50,16 +50,16 @@ impl NetMonitor {
 
     pub fn start(&self) {
         if cfg!(target_os = "linux") {
-            let self_clone = self.clone();
+            let buffer = self.buffer.clone();
             tokio::spawn(async move {
                 let mut last: u64 = 0;
                 loop {
                     {
-                        let new = self_clone.fetch_current_value();
+                        let new = fetch_current_value();
                         if last != 0 {
                             let system_time = SystemTime::now();
                             let datetime: DateTime<Local> = system_time.into();
-                            let mut buffer = self_clone.buffer.write().await;
+                            let mut buffer = buffer.write().await;
                             buffer.push_back(TimeValue::new(
                                 datetime.format("%H:%M:%S").to_string(),
                                 (new - last) * 8 / INTERVAL_SECONDS,
@@ -75,48 +75,48 @@ impl NetMonitor {
             });
         }
     }
+}
 
-    #[cfg(feature = "bpf")]
-    pub fn fetch_current_value(&self) -> u64 {
-        self.socket_filter.get_current_outbound_bytes()
-    }
+#[cfg(feature = "bpf")]
+pub fn fetch_current_value() -> u64 {
+    SOCKET_FILTER.get_current_outbound_bytes()
+}
 
-    // Inter-|   Receive                                                |  Transmit
-    //      face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
-    //         lo: 199123505  183957    0    0    0     0          0         0 199123505  183957    0    0    0     0       0          0
-    //       ens5: 194703959  424303    0    0    0     0          0         0 271636211  425623    0    0    0     0       0          0
-    #[cfg(not(feature = "bpf"))]
-    pub fn fetch_current_value(&self) -> u64 {
-        use std::fs;
-        if let Ok(mut content) = fs::read_to_string("/proc/net/dev") {
-            content = content.replace("\r\n", "\n");
-            let strs = content.split('\n');
-            let mut new: u64 = 0;
-            for str in strs {
-                let array: Vec<&str> = str.split_whitespace().collect();
+// Inter-|   Receive                                                |  Transmit
+//      face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+//         lo: 199123505  183957    0    0    0     0          0         0 199123505  183957    0    0    0     0       0          0
+//       ens5: 194703959  424303    0    0    0     0          0         0 271636211  425623    0    0    0     0       0          0
+#[cfg(not(feature = "bpf"))]
+pub fn fetch_current_value() -> u64 {
+    use std::fs;
+    if let Ok(mut content) = fs::read_to_string("/proc/net/dev") {
+        content = content.replace("\r\n", "\n");
+        let strs = content.split('\n');
+        let mut new: u64 = 0;
+        for str in strs {
+            let array: Vec<&str> = str.split_whitespace().collect();
 
-                if array.len() == 17 {
-                    if *array.first().unwrap_or(&"") == "lo:" {
-                        continue;
-                    }
-                    if array.first().unwrap_or(&"").starts_with("veth") {
-                        continue;
-                    }
-                    if array.first().unwrap_or(&"").starts_with("flannel") {
-                        continue;
-                    }
-                    if array.first().unwrap_or(&"").starts_with("cni0") {
-                        continue;
-                    }
-                    if array.first().unwrap_or(&"").starts_with("utun") {
-                        continue;
-                    }
-                    new += array.get(9).unwrap_or(&"").parse::<u64>().unwrap_or(0);
+            if array.len() == 17 {
+                if *array.first().unwrap_or(&"") == "lo:" {
+                    continue;
                 }
+                if array.first().unwrap_or(&"").starts_with("veth") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("flannel") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("cni0") {
+                    continue;
+                }
+                if array.first().unwrap_or(&"").starts_with("utun") {
+                    continue;
+                }
+                new += array.get(9).unwrap_or(&"").parse::<u64>().unwrap_or(0);
             }
-            new
-        } else {
-            0
         }
+        new
+    } else {
+        0
     }
 }
