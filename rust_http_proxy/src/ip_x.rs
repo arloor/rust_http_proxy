1c7b5ac4975fdeb5be4dd8c1a821471c5cdcb49e
arloor <admin@arloor.com>
2024-07-02 21:10:56 +0800

    refactor: Update ip_x.rs to fix formatting of socket address

--- rust_http_proxy/src/ip_x.rs
+++ rust_http_proxy/src/ip_x.rs
@@ -75,7 +75,7 @@ impl<'a> std::fmt::Display for FormatAddr<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "https://ip.im/{}  {}",
+            "https://ip.im/{} {}",
             self.0.ip().to_canonical(),
             self.0.port()
         )
