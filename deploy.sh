5795e73ae68caa420d7477e96c5fc2701ec4c770
arloor <admin@arloor.com>
2024-07-04 19:21:59 +0800

    refactor: Update deploy.sh to include new host hkk.arloor.dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-hosts="hk.arloor.dev sg.arloor.dev us.arloor.dev bwg.arloor.dev gg.arloor.dev pl.arloor.com ti.arloor.com li.arloor.dev hi.arloor.dev"
+hosts="hk.arloor.dev hkk.arloor.dev sg.arloor.dev us.arloor.dev bwg.arloor.dev gg.arloor.dev pl.arloor.com ti.arloor.com li.arloor.dev hi.arloor.dev"
 for i in ${hosts}; do
     ssh -o StrictHostKeyChecking=no root@${i} '
             hostname;
