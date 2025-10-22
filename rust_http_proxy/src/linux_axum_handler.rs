19440f9dfd449cf5433e627b31eacbd23dd8ba25
arloor <admin@arloor.com>
2025-10-22 19:42:38 +0800

    调整连接字符串格式，增加输出对齐，以改善入站和出站连接信息的可读性

--- rust_http_proxy/src/linux_axum_handler.rs
+++ rust_http_proxy/src/linux_axum_handler.rs
@@ -92,10 +92,10 @@ async fn count_stream(socket_direction: SocketDirection) -> Result<(HeaderMap, S
             for (peer_addr, peer_port, local_addr, local_port, process_info) in connections {
                 let connection_str = match socket_direction {
                     SocketDirection::Incoming => {
-                        format!("{peer_addr:>15}   => {local_addr:>15}:{local_port:<5} {process_info}")
+                        format!("{peer_addr:>39}   => {local_addr:>39}:{local_port:<5} {process_info}")
                     }
                     SocketDirection::Outgoing => {
-                        format!("{local_addr:>15}   => {peer_addr:>15}:{peer_port:<5} {process_info}")
+                        format!("{local_addr:>39}   => {peer_addr:>39}:{peer_port:<5} {process_info}")
                     }
                 };
                 *connection_counts.entry(connection_str).or_insert(0) += 1;
