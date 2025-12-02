88193da6a80882012e45ae1095925366a98274d1
arloor <admin@arloor.com>
2025-12-02 17:09:37 +0800

    移除不必要的 IPv6 转换函数，简化代码结构

--- rust_http_proxy/src/ip_x.rs
+++ rust_http_proxy/src/ip_x.rs
@@ -1,28 +1,4 @@
 use std::io;
-use std::net::IpAddr;
-use std::net::Ipv4Addr;
-
-pub(crate) fn _ipv6_mapped_to_ipv4(addr: IpAddr) -> IpAddr {
-    match addr {
-        IpAddr::V6(v6_addr) => {
-            if v6_addr.segments()[..6] == [0, 0, 0, 0, 0, 0xFFFF] {
-                #[cfg(debug_assertions)]
-                log::info!("found IPv4-mapped IPv6 address: \"{addr}\", converting to IPv4");
-                // 提取IPv4部分并转换为SocketAddr::V4
-                let ip4_bits: [u16; 2] = [v6_addr.segments()[6], v6_addr.segments()[7]];
-                Ipv4Addr::new((ip4_bits[0] >> 8) as u8, ip4_bits[0] as u8, (ip4_bits[1] >> 8) as u8, ip4_bits[1] as u8)
-                    .into()
-            } else {
-                // 不是IPv4映射的IPv6地址，直接返回原地址
-                addr
-            }
-        }
-        IpAddr::V4(_) => {
-            // 已经是IPv4地址，直接返回
-            addr
-        }
-    }
-}
 
 #[cfg(not(feature = "pnet"))]
 pub fn local_ip() -> io::Result<String> {
