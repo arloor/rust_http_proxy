a1a75194093720e2d743a76577c5e3005f49799a
arloor <admin@arloor.com>
2025-12-13 10:27:34 +0800

    优化 deploy.sh 中的 SSH 命令格式，提升可读性

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ podman push quay.io/arloor/rust_http_proxy:bpf
 #! /bin/bash
 hosts="wee.arloor.dev hk.arloor.dev us.arloor.dev hi.arloor.dev ttl.arloor.com xq.arloor.com ti.arloor.com"
 for i in ${hosts}; do
-    ssh -o StrictHostKeyChecking=no root@${i} '
-            . pass
-            hostname;
-            systemctl restart proxy;
-            podman rmi -a 2>/dev/null
-            podman images --digests |grep arloor/rust_http_proxy|awk "{print \$4\" \"\$3}";
-            '
+    ssh -o StrictHostKeyChecking=no root@${i} <<'EOF'
+        . pass
+        hostname;
+        systemctl restart proxy;
+        podman rmi -a 2>/dev/null
+        podman images --digests |grep arloor/rust_http_proxy|awk "{print \$4\" \"\$3}";
+EOF
 done
